b2b856532cfc46c22c51dfb35f34e7291b83b001
dogus ural <dogus@riddleandcode.com>
2019-10-02 15:16:00 +0200

    added random test case

--- src/main.c
+++ src/main.c
@@ -10,7 +10,13 @@ do
 } while (result != SE_SUCCESS);
 
 printf("\n\t\t... SECURE ELEMENT has been initialized succesfully ...\n\n");
-
+uint8_t rnd[32]= {0};
+if(se_get_random(rnd,32) == SE_SUCCESS)
+for (size_t i = 0; i < 32; i++)
+{
+printf("%02x ",rnd[i]);
+}
+puts("");
 
 
 
