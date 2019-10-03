7d662ed9415735cfc163506ae4832019fd61a65f
dogus ural <dogus@riddleandcode.com>
2019-10-03 10:07:30 +0200

    removed library paths from toolchain file

--- toolchain-rpi.cmake
+++ toolchain-rpi.cmake
@@ -9,5 +9,5 @@ SET(CMAKE_FIND_ROOT_PATH $ENV{RPI_PATH}/arm-linux-gnueabihf/sysroot/)
 # Use our definitions for compiler tools
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # Search for libraries and headers in the target directories only
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+#SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
