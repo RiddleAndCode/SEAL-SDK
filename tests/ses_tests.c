b245d15bc395f9498c2ccc54cd5293c3e2b245b3
dogus ural <dogus@riddleandcode.com>
2019-11-21 14:51:41 +0100

    new tests , and updated libs

--- tests/ses_tests.c
+++ tests/ses_tests.c
@@ -10,10 +10,14 @@
 #include <string.h>
 #include "unity.h"
 
+uint8_t test_pin_0[]={'R','I','D','D','L','E','&','C','O','D','E','R','O','C','K','S','!'};
+uint8_t test_pin_1[]={'C','+','+',' ','I','S',' ','N','O','T',' ','C'};
+uint8_t test_pin_2[]={'P','E','E','R','T','O','P','E','E','R','E','L','E','C','T','R','O','N','I','C','K','C','A','S','H','S','Y','S','T','E','M'};
 
-#define PRINT_AUTHENTICATION_LOG()   (i > 4 ? (i == 5 ? printf("\tAuthenticating memory slot %d with key in slot (1) \n",i) \
-: (i == 6 ? printf("\tAuthenticating memory slot %d with key in slot (2) \n",i) :\
- printf("\tAuthenticating memory slot %d with key in slot (0) \n",i)) ):\
+
+#define PRINT_AUTHENTICATION_LOG()   (i > 4 ? (i == 5 ? printf("\tAuthenticating memory slot %d with key in slot (0) \n",i) \
+: (i == 6 ? printf("\tAuthenticating memory slot %d with key in slot (1) \n",i) :\
+ printf("\tAuthenticating memory slot %d with key in slot (2) \n",i)) ):\
   printf("\tMemory slot %d doesn't require authentication\n",i))
 
 
@@ -25,7 +29,7 @@ int main(void) {
 
 
   TEST_ASSERT_EQUAL(SE_SUCCESS,se_secure_storage_init());
-  TEST_ASSERT_EQUAL(SE_SUCCESS,se_secure_storage_personalize(false));
+  TEST_ASSERT_EQUAL(SE_SUCCESS,se_secure_storage_personalize(false,test_pin_0,test_pin_1,test_pin_2));
 
   UNITY_BEGIN();
   RUN_TEST(test_write_data);
@@ -47,14 +51,19 @@ void test_write_data()
   memset(write_buffer+(SIZE/4),0xEF,SIZE/4);
   memset(write_buffer+(SIZE/2),0xCA,SIZE/4);
   memset(write_buffer+(3*SIZE/4),0xFE,SIZE/4);
-
+  uint8_t* key_ptr = NULL;
 for (size_t i = 0; i < ZONE; i++)
 {
   printf("\n\n\tRunning the READ/WRITE tests for memory slot (%d-%d)\n",i,ZONE-1);
   PRINT_AUTHENTICATION_LOG();
   printf("\tWriting %d bytes of data\n\n",SIZE);
-
-  TEST_ASSERT_EQUAL(SE_SUCCESS,se_authenticate(i));
+  if(i == 5)
+    key_ptr = test_pin_0;
+  else if(i == 6)
+    key_ptr = test_pin_1;
+  else if(i > 6  && i < 16)
+    key_ptr = test_pin_2;
+  TEST_ASSERT_EQUAL(SE_SUCCESS,se_authenticate(i,key_ptr));
   TEST_ASSERT_EQUAL(SE_SUCCESS, se_secure_store(i,write_buffer,SIZE)) ;
 
   TEST_ASSERT_EQUAL(SE_SUCCESS,se_secure_read(i,data,SIZE)) ;
@@ -70,4 +79,15 @@ for (size_t i = 0; i < ZONE; i++)
 
   memset(data,0,SIZE);
 }
+printf("\n\n\tAttemting to authenticate slot 5 with the Key number 1, Should Fail ... \n");
+TEST_ASSERT_NOT_EQUAL(SE_SUCCESS,se_authenticate(5,test_pin_1));
+printf("\n\n\tAuthenticating slot 5 with the Key number 0, Should Pass ... \n");
+TEST_ASSERT_EQUAL(SE_SUCCESS,se_authenticate(5,test_pin_0));
+printf("\n\n\tAttemting to write to slot 6 when chip is authenticated for slot 5 , Should Fail ... \n");
+TEST_ASSERT_NOT_EQUAL(SE_SUCCESS, se_secure_store(6,write_buffer,SIZE)) ;
+printf("\n\n\tAttemting to authenticate slot 6 with the Key number 2, Should Fail ... \n");
+TEST_ASSERT_NOT_EQUAL(SE_SUCCESS,se_authenticate(6,test_pin_2));
+printf("\n\n\tAttemting to authenticate slot 10 with the Key number 0, Should Fail ... \n");
+TEST_ASSERT_NOT_EQUAL(SE_SUCCESS,se_authenticate(10,test_pin_0));
+
 }
--- tests/v1_tests.c
+++ tests/v1_tests.c
@@ -13,7 +13,7 @@ void test_verify();
 void test_store();
 void test_save_pubkey();
 void test_hash();
-uint8_t publicKeys[3][64]=  {{0}};
+uint8_t publicKeys[10][64]=  {{0}};
 uint8_t SHA_CMP[] = {84 ,191, 102, 155, 32, 201, 150, 11, 12, 62, 159, 97, 247, 24, 142, 214, 198, 97, 29, 98, 68, 184, 26, 69, 252, 199, 105, 185, 11, 90, 119, 88};
 
 uint8_t testText[] = "Smart cities combine the Internet of Things with almost every aspect of life,"
@@ -40,7 +40,6 @@ int main()
     RUN_TEST(test_random);
     RUN_TEST(test_generate);
     RUN_TEST(test_read_pubkey);
-    RUN_TEST(test_save_pubkey);
     RUN_TEST(test_store);
     RUN_TEST(test_verify);
     RUN_TEST(test_hash);
@@ -66,83 +65,107 @@ void test_random()
 
 void test_generate()
 {
+    #define NUMBER_OF_KEYS 10
     printf("\n\r\t... Running Generate Key-Pair Test ...\n\n\r");
     TEST_ASSERT_EQUAL(SE_SUCCESS,se_init(ESTABLISH_SCI2C));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_generate_keypair(0));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_generate_keypair(2));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_generate_keypair(7));
+    for (size_t i = 1; i < NUMBER_OF_KEYS; i++)
+    {
+        TEST_ASSERT_EQUAL(SE_SUCCESS,se_generate_keypair(i));
+
+    }
+
     TEST_ASSERT_EQUAL(SE_SUCCESS,se_close());
 }
 
 void test_read_pubkey()
 {
+    #define NUMBER_OF_KEYS 10
     printf("\n\r\t... Running Read Public Key Test ...\n\n\r");
     TEST_ASSERT_EQUAL(SE_SUCCESS,se_init(ESTABLISH_SCI2C));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_get_pubkey(0,publicKeys[0],NULL));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_get_pubkey(2,publicKeys[1],NULL));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_get_pubkey(7,publicKeys[2],NULL));
+
+    for (size_t i = 0; i < NUMBER_OF_KEYS; i++)
+    {
+        TEST_ASSERT_EQUAL(SE_SUCCESS,se_get_pubkey(i,publicKeys[i],NULL));
+    }
+
     TEST_ASSERT_EQUAL(SE_SUCCESS,se_close());
 }
 void test_verify()
 {
+    #define NUMBER_OF_KEYS 10
+
     uint8_t input[] = "Hello world!!";
     uint8_t sha[32] = {0};
     uint16_t shaLen = 32;
-    uint8_t signature[3][64] =  {{0}};
+    uint8_t signature[NUMBER_OF_KEYS][64] =  {{0}};
     printf("\n\r\t... Running Verify Test ...\n\n\r");
     TEST_ASSERT_EQUAL(SE_SUCCESS,se_init(ESTABLISH_SCI2C));
     TEST_ASSERT_EQUAL(SE_SUCCESS,se_get_sha256(input,sizeof(input),sha,&shaLen));
 
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_sign(0,sha,0,signature[0],0));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_sign(2,sha,0,signature[1],0));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_sign(7,sha,0,signature[2],0));
 
-    TEST_ASSERT_EQUAL(SE_VERIFY_SUCCESS,se_verify_external(0,publicKeys[0],0,sha,0,signature[0],0));
-    TEST_ASSERT_EQUAL(SE_VERIFY_SUCCESS,se_verify_external(0,publicKeys[1],0,sha,0,signature[1],0));
-    TEST_ASSERT_EQUAL(SE_VERIFY_SUCCESS,se_verify_external(0,publicKeys[2],0,sha,0,signature[2],0));
+    for (size_t i = 0; i < NUMBER_OF_KEYS; i++)
+    {
+        TEST_ASSERT_EQUAL(SE_SUCCESS,se_sign(i,sha,0,signature[i],0));
+    }
 
-    TEST_ASSERT_EQUAL(SE_VERIFY_FAIL,se_verify_external(0,publicKeys[0],0,sha+1,0,signature[0],0));
-    TEST_ASSERT_EQUAL(SE_VERIFY_FAIL,se_verify_external(0,publicKeys[1],0,sha+1,0,signature[1],0));
-    TEST_ASSERT_EQUAL(SE_VERIFY_FAIL,se_verify_external(0,publicKeys[2],0,sha+1,0,signature[2],0));
+    for (size_t i = 0; i < NUMBER_OF_KEYS; i++)
+    {
+        TEST_ASSERT_EQUAL(SE_VERIFY_SUCCESS,se_verify_external(0,publicKeys[i],0,sha,0,signature[i],0));
+    }
+
+    for (size_t i = 0; i < NUMBER_OF_KEYS; i++)
+    {
+        TEST_ASSERT_EQUAL(SE_VERIFY_FAIL,se_verify_external(0,publicKeys[i],0,sha+1,0,signature[i],0));
+    }
+
     TEST_ASSERT_EQUAL(SE_SUCCESS,se_close());
 
 }
 
 void test_store()
 {
+    #define STORE_SLOT_1 10
+    #define STORE_SLOT_2 (STORE_SLOT_1 + 1)
+    #define STORE_SLOT_3 (STORE_SLOT_2 + 1)
+    #define STORE_SLOT_4 (STORE_SLOT_3 + 1)
+    #define STORE_SLOT_5 (STORE_SLOT_4 + 1)
+    #define STORE_SLOT_6 (STORE_SLOT_5 + 1)
+
     printf("\n\r\t... Running Store Test ...\n\n\r");
     uint8_t write_data[72] = {0};
     uint8_t read_data[72] = {0};
     memset(write_data,'x',72);
     TEST_ASSERT_EQUAL(SE_SUCCESS,se_init(ESTABLISH_SCI2C));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_write_data(0,write_data,72));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_read_data(0,read_data,72));
+
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_write_data(STORE_SLOT_1,write_data,72));
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_read_data(STORE_SLOT_1,read_data,72));
+    TEST_ASSERT_EQUAL(0,memcmp(write_data,read_data,72));
+    memset(read_data,0,72);
+
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_write_data(STORE_SLOT_2,write_data,72));
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_read_data(STORE_SLOT_2,read_data,72));
+    TEST_ASSERT_EQUAL(0,memcmp(write_data,read_data,72));
+    memset(read_data,0,72);
+
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_write_data(STORE_SLOT_3,write_data,72));
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_read_data(STORE_SLOT_3,read_data,72));
+    TEST_ASSERT_EQUAL(0,memcmp(write_data,read_data,72));
+    memset(read_data,0,72);
+
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_write_data(STORE_SLOT_4,write_data,72));
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_read_data(STORE_SLOT_4,read_data,72));
+    TEST_ASSERT_EQUAL(0,memcmp(write_data,read_data,72));
+    memset(read_data,0,72);
+
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_write_data(STORE_SLOT_5,write_data,72));
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_read_data(STORE_SLOT_5,read_data,72));
+    TEST_ASSERT_EQUAL(0,memcmp(write_data,read_data,72));
+    memset(read_data,0,72);
+
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_write_data(STORE_SLOT_6,write_data,72));
+    TEST_ASSERT_EQUAL(SE_SUCCESS,se_read_data(STORE_SLOT_6,read_data,72));
     TEST_ASSERT_EQUAL(0,memcmp(write_data,read_data,72));
     TEST_ASSERT_EQUAL(SE_SUCCESS,se_close());
-
-}
-void test_save_pubkey()
-{
-    printf("\n\r\t... Running Store Public Key Test ...\n\n\r");
-    uint8_t PUB[64] = {0};
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_init(ESTABLISH_SCI2C));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_save_key_pair(10,publicKeys[0],0,0,0));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_save_key_pair(11,publicKeys[0],0,0,0));
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_save_key_pair(12,publicKeys[0],0,0,0));
-
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_get_pubkey(10,PUB,0));
-    TEST_ASSERT_EQUAL(0,memcmp(PUB,publicKeys[0],64));
-    memset(PUB,0,64);
-
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_get_pubkey(11,PUB,0));
-    TEST_ASSERT_EQUAL(0,memcmp(PUB,publicKeys[0],64));
-    memset(PUB,0,64);
-
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_get_pubkey(12,PUB,0));
-    TEST_ASSERT_EQUAL(0,memcmp(PUB,publicKeys[0],64));
-
-    TEST_ASSERT_EQUAL(SE_SUCCESS,se_close());
-
 
 }
 
